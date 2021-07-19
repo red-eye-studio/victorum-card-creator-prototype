f18704dee27748c6e2d909a0cb0993a8004591b4
aidant <15520814+aidant@users.noreply.github.com>
2021-07-19 23:44:18 +1000

    Update Icons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -3,7 +3,7 @@
     class="flex flex-row gap-1 items-center min-w-max p-2 border-2 rounded x-focus"
     @click="() => emit('click')"
   >
-    <img :src="'/assets/icon-' + props.icon + '.svg'" class="h-5 inline" aria-hidden />
+    <img :src="'./assets/icon-' + props.icon + '.svg'" class="h-5 inline" aria-hidden />
     <span class="align-middle">{{ props.label }}</span>
   </button>
 </template>
